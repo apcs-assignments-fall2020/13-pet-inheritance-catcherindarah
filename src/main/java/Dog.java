e49917277b36fc4080ef7b39df14162e41337143
catcherindarah <22rray@athenian.org>
2020-12-16 00:38:33 +0000

    finished the assignment

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -1,18 +1,45 @@
 public class Dog extends Pet {
     // Instance variable(s)
-
+    private String breed;
 
     // Constructors
-    
+    public Dog()
+    {
+        super("Rahil", 100);
+        this.breed = "lab";
+    }
+    public Dog(String name, int age, String breed)
+    {
+        super(name,age);
+        this.breed = breed;
+    }
 
     // makeNoise() method
+    public void makeNoise()
+    {
+        System.out.println("Woof!");
+    }
 
 
     // toString method
-
+    public String toString()
+    {
+        String str = "Name: " + this.getName() + ", Age: " + this.getAge() + ", Breed: " + breed;
+        return str;
+    }
 
     // Getter
-
+    public String getBreed() 
+    {
+        return this.breed;
+    }
 
     // Setter
+    public void setBreed(String breed)
+    {
+        if(breed.trim().length() != 0)
+        {
+            this.breed = breed;
+        }
+    }
 }
